6b439629b6fc0c8862d125e212b3f7dfe7614295
kellan <kellan@pobox.com>
2024-05-26 16:56:07 -0400

    tweak

--- sql/pugsql.sql
+++ sql/pugsql.sql
@@ -6,14 +6,14 @@ order by ts desc
 limit :count;
 
 -- :name create_links_tables 
-CREATE TABLE IF NOT EXISTS links (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    ts INTEGER,  -- seconds since unix epoch
-    url TEXT,
-    description TEXT,
-    extended TEXT,
-    tags TEXT,  -- space delimited
-    hash TEXT unique)
+create table if not exists links (
+    id integer primary key autoincrement,
+    ts integer,  -- seconds since unix epoch
+    url text,
+    description text,
+    extended text,
+    tags text,  -- space delimited
+    hash text unique)
 
 -- :name upsert_link :insert
 insert or replace into links
@@ -23,10 +23,10 @@ values
 
 
 -- :name distinct_year_months :many
-select DISTINCT strftime('%Y-%m', ts, 'unixepoch') as year_month from links;
+select distinct strftime('%y-%m', ts, 'unixepoch') as year_month from links;
 
 -- :name select_by_year_month :many
-SELECT * FROM links WHERE strftime('%Y-%m', ts, 'unixepoch') = :year_month;
+select * from links where strftime('%y-%m', ts, 'unixepoch') = :year_month;
 
 -- :name latest_ts :scalar
 select max(ts) from links;
