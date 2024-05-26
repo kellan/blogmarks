9ba3c3e870601e41fb550eee18def0ca61d86031
kellan <kellan@pobox.com>
2024-05-26 15:20:40 -0400

    select by year-month

--- sql/pugsql.sql
+++ sql/pugsql.sql
@@ -25,5 +25,8 @@ on conflict do nothing;
 -- :name distinct_year_months :many
 select DISTINCT strftime('%Y-%m', ts, 'unixepoch') as year_month from links;
 
+-- :name select_by_year_month :many
+SELECT * FROM links WHERE strftime('%Y-%m', ts, 'unixepoch') = :year_month;
+
 -- :name latest_ts :scalar
 select max(ts) from links;
