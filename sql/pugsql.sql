f9ddd046c4d2c0565e6c36d0b5aed8c06fbffe70
kellan <kellan@pobox.com>
2024-05-28 06:48:00 -0400

    whoops, case matters

--- sql/pugsql.sql
+++ sql/pugsql.sql
@@ -23,10 +23,10 @@ values
 
 
 -- :name distinct_year_months :many
-select distinct strftime('%y-%m', ts, 'unixepoch') as year_month from links;
+select distinct strftime('%Y-%m', ts, 'unixepoch') as year_month from links;
 
 -- :name select_by_year_month :many
-select * from links where strftime('%y-%m', ts, 'unixepoch') = :year_month;
+select * from links where strftime('%Y-%m', ts, 'unixepoch') = :year_month;
 
 -- :name latest_ts :scalar
 select max(ts) from links;
